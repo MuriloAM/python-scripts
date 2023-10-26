66e0744d1419715765585faf57c215fa38d1d349
MuriloAM <muriloa.melo01@gmail.com>
2023-10-26 13:50:26 -0300

    feat: :sparkles: add constants write method

--- dwin/dwin_tool.py
+++ dwin/dwin_tool.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python
-from dwin_format import dwin_make_frame
+from dwin_format import dwin_serialize
+from time import sleep
 
 class Dwin:
+    DWIN_WRITE = "82"
+    DWIN_READ = "83"
+
     def __init__(self, width, height, port):
         self.width = width
         self.height = height
         self.port = port
     
+    def write(self, addr, data):
+        msg = dwin_serialize("82", addr, data)
 
-def main():
-    dwin = Dwin(800, 480, "COM6")
+        pass
 
 if __name__ == "__main__":
-    main()
+    def main(addr, data):
+        print("running main()")
+        # dwin = Dwin(800, 480, "COM6")
+        # dwin_msg = dwin_serialize(addr, data)
+    
+    dwin_addr = "5000"
+    dwin_data = ""
+    main(dwin_addr, dwin_data)
