efab2c585cfaec2016f87a0970aa5fc38d674daa
MuriloAM <muriloa.melo01@gmail.com>
2023-10-27 10:45:52 -0300

    feat: :sparkles: add connect and desconnect method

--- dwin/dwin_conn.py
+++ dwin/dwin_conn.py
@@ -4,6 +4,7 @@ import glob
 import serial
 from time import sleep
 
+serialPort = []
 
 def serial_ports():
     """ Lists serial port names
@@ -33,82 +34,113 @@ def serial_ports():
             pass
     return result
 
-if __name__ == "__main__":
-    def write_frame(addr, data):
-        HEADER = "5AA5"
-        if data < 16:
-            data = ("0{0:X}".format(data))
-        else:
-            data = ("{0:X}".format(data))
-        
-        body_data = "82{}{}".format(addr, data)
-        body_len = int(len(body_data) / 2)
-
-        if body_len < 16:
-            body_len = ("0{0:X}".format(body_len))
-        else:
-            body_len = ("{0:X}".format(body_len))
-
-        msg_frame = "{}{}{}".format(HEADER, body_len, body_data)
-        
-        return msg_frame
+def connect(port, baud):
+    try_connect = 0
+    global serialPort
+    print("connecting to dwin-display on port:{}".format(port))
+    serialPort = serial.Serial(port=port, baudrate=baud)
     
-    def main(port):
-
-        serialPort = serial.Serial()
-        
-        serialPort.port =  port
-        serialPort.baudrate = 115200
-        serialPort.bytesize = serial.EIGHTBITS
-        serialPort.parity = serial.PARITY_NONE
-        serialPort.stopbits = serial.STOPBITS_ONE
-        serialPort.timeout = .1
-
-        while not serialPort.is_open:
-            print("port:{} disconnected, try to reconnect...".format(serialPort.port))
-            serialPort.open()
+    while not serialPort.is_open:
+        print("connecting...")
+        serialPort.open()
+        if try_connect < 5:
+            try_connect += 1
             sleep(0.5)
+        else:
+            return False
+    print("connected!")
+    return True
 
-        try:
-            serialPort.flushInput()
-            serialPort.flushOutput()
-            print("name:{}, port:{} is connected, send a message".format(serialPort.name, serialPort.port))
+def disconnect():
+    print("\nclosing serialPort:{}".format(serialPort.port))
+    serialPort.close()
 
-            val = 0
+if __name__ == "__main__":
+    
+    try:
+        print("OS:{}".format(sys.platform))
+        print("available com port:{}".format(serial_ports()))
+        connect("/dev/ttyUSB0", 115200)
 
-            while True:
-                msg_in = serialPort.readline()
-                
-                if msg_in != b'':
-                    # msg_in = msg_in.upper()
-                    # msg_in = int(msg_in)
-                    # my_str = str(msg_in, 'utf-8')
-                    # print(type(msg_in))
-                    print("rx:{}".format(msg_in.hex()))
-                    # print(my_str)
-                    # print("data_raw:{}".format(data_raw))
-                    # break
-                elif val < 10:
-                    hex_string = write_frame(5002, val)
-                    # print("tx:{}".format(hex_string))
-                    # prepara str to hex format
-                    hex_bytes = bytes.fromhex(hex_string)
-                    # serialPort.write(hex_bytes)
-                    # 5AA5 06 83 5000 01 CABA
-                    # 5a69204fe9
-
-                    val = val + 1
-                    sleep(0.1)
-                
-        except KeyboardInterrupt:
-            print("\nclosing serialPort:{}".format(serialPort.port))
-            serialPort.close()
+        while True:
+            sleep(1)
+    except:
+        print("\nexception ocurr, closing serialPort:{}".format(serialPort.port))
+        serialPort.close()
+    finally:
+        print("\nscript done, closing serialPort:{}".format(serialPort.port))
+        serialPort.close()
+    
+    # def write_frame(addr, data):
+    #     HEADER = "5AA5"
+    #     if data < 16:
+    #         data = ("0{0:X}".format(data))
+    #     else:
+    #         data = ("{0:X}".format(data))
         
-        finally:
-            print("\ntranmission done closing serialPort:{}".format(serialPort.port))
-            serialPort.close()
+    #     body_data = "82{}{}".format(addr, data)
+    #     body_len = int(len(body_data) / 2)
+
+    #     if body_len < 16:
+    #         body_len = ("0{0:X}".format(body_len))
+    #     else:
+    #         body_len = ("{0:X}".format(body_len))
+
+    #     msg_frame = "{}{}{}".format(HEADER, body_len, body_data)
+        
+    #     return msg_frame
     
-    print("OS:{}".format(sys.platform))
-    print("available com port:{}".format(serial_ports()))
-    main("/dev/ttyUSB0")
-    
+    # def main(port):
+
+    #     serialPort = serial.Serial()
+        
+    #     serialPort.port =  port
+    #     serialPort.baudrate = 115200
+    #     serialPort.bytesize = serial.EIGHTBITS
+    #     serialPort.parity = serial.PARITY_NONE
+    #     serialPort.stopbits = serial.STOPBITS_ONE
+    #     serialPort.timeout = .1
+
+    #     while not serialPort.is_open:
+    #         print("port:{} disconnected, try to reconnect...".format(serialPort.port))
+    #         serialPort.open()
+    #         sleep(0.5)
+
+    #     try:
+    #         serialPort.flushInput()
+    #         serialPort.flushOutput()
+    #         print("name:{}, port:{} is connected, send a message".format(serialPort.name, serialPort.port))
+
+    #         val = 0
+
+    #         while True:
+    #             msg_in = serialPort.readline()
+                
+    #             if msg_in != b'':
+    #                 # msg_in = msg_in.upper()
+    #                 # msg_in = int(msg_in)
+    #                 # my_str = str(msg_in, 'utf-8')
+    #                 # print(type(msg_in))
+    #                 print("rx:{}".format(msg_in.hex()))
+    #                 # print(my_str)
+    #                 # print("data_raw:{}".format(data_raw))
+    #                 # break
+    #             elif val < 10:
+    #                 hex_string = write_frame(5002, val)
+    #                 # print("tx:{}".format(hex_string))
+    #                 # prepara str to hex format
+    #                 hex_bytes = bytes.fromhex(hex_string)
+    #                 # serialPort.write(hex_bytes)
+    #                 # 5AA5 06 83 5000 01 CABA
+    #                 # 5a69204fe9
+
+    #                 val = val + 1
+    #                 sleep(0.1)
+                
+    #     except KeyboardInterrupt:
+    #         print("\nclosing serialPort:{}".format(serialPort.port))
+    #         serialPort.close()
+        
+    #     finally:
+    #         print("\ntranmission done closing serialPort:{}".format(serialPort.port))
+    #         serialPort.close()
