a5b9cfafccf2d0cde22f3faf61fc0be80c092290
MuriloAM <muriloa.melo01@gmail.com>
2023-10-30 08:44:04 -0300

    feat: :bug: fixed serial rx readline
    serial readline was without timeout and blocking application

--- dwin/dwin_conn.py
+++ dwin/dwin_conn.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python
 import sys
 import glob
-import pickle
 import serial
 import serial.tools.list_ports as port_list
 from time import sleep
-
-def search_comport(comport):
-    # print("search for COMx ports...")
-    # ports = list(port_list.comports())
-    # for p in ports:
-    #     print("{}".format(ports))
-    #     if ports == comport:
-    #         return True
-    # if comport isn't found in ports return False
-    return False
 
 def serial_ports():
     """ Lists serial port names
@@ -64,16 +53,9 @@ if __name__ == "__main__":
         
         return msg_frame
     
-    def main(port):
+    def main(port, baud):
 
-        serialPort = serial.Serial()
-        
-        serialPort.port =  port
-        serialPort.baudrate = 115200
-        serialPort.bytesize = serial.EIGHTBITS
-        serialPort.parity = serial.PARITY_NONE
-        serialPort.stopbits = serial.STOPBITS_ONE
-        serialPort.timeout = .1
+        serialPort = serial.Serial(port, baud, timeout= 0.1)
 
         while not serialPort.is_open:
             print("port:{} disconnected, try to reconnect...".format(serialPort.port))
@@ -91,25 +73,15 @@ if __name__ == "__main__":
                 msg_in = serialPort.readline()
                 
                 if msg_in != b'':
-                    # msg_in = msg_in.upper()
-                    # msg_in = int(msg_in)
-                    # my_str = str(msg_in, 'utf-8')
-                    # print(type(msg_in))
-                    print("rx:{}".format(msg_in.hex()))
-                    # print(my_str)
-                    # print("data_raw:{}".format(data_raw))
-                    # break
+                    print("rx:{}".format(msg_in.hex().upper()))
                 elif val < 10:
                     hex_string = write_frame(5002, val)
-                    # print("tx:{}".format(hex_string))
+                    print("tx:{}".format(hex_string))
                     # prepara str to hex format
                     hex_bytes = bytes.fromhex(hex_string)
-                    # serialPort.write(hex_bytes)
-                    # 5AA5 06 83 5000 01 CABA
-                    # 5a69204fe9
-
+                    serialPort.write(hex_bytes)
                     val = val + 1
-                    sleep(0.1)
+                    sleep(1)
                 
         except KeyboardInterrupt:
             print("\nclosing serialPort:{}".format(serialPort.port))
@@ -121,5 +93,4 @@ if __name__ == "__main__":
     
     print("OS:{}".format(sys.platform))
     print("available com port:{}".format(serial_ports()))
-    main("/dev/ttyUSB0")
-    
+    main("/dev/ttyUSB0", 115200)
