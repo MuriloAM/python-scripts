14863b0659da44a1ec672867f6f50dacdab15632
MuriloAM <muriloa.melo01@gmail.com>
2023-10-31 10:35:38 -0300

    feat: :sparkles: dwin logic to test update and write confirmation methods

--- dwin/dwin.py
+++ dwin/dwin.py
@@ -1,7 +1,26 @@
 from dwin_conn import *
+from time import sleep
 
 display = DwinConn("/dev/ttyUSB0", 115200)
-display.write("5000", "61686564")
-rx = display.read("5000", "7D")
-print("rx:{}".format(rx))
-del display
+try:
+    while True:
+        rx = display.update()
+        if rx is not None:
+            print("response:{}".format(rx))
+            # rx = None
+            # break
+        else:
+            # tx = "61686564"
+            # print("tx:{}".format(tx))
+            err = display.write("5000", "61686564")
+            if err == "5AA503824F4B":
+                print("write err:OK")
+            else:
+                print("write err:{}".format(err))
+        sleep(1)
+        # rx = display.read("5000", "7D")
+        # print("rx:{}".format(rx))
+        
+except:
+    print("finalizando diplay")
+    del display
